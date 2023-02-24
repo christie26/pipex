f615008dfd3eb187c63e18df4686cd77914d908a
yoonsele <yoonseo9800@gmail.com>
2023-02-24 19:48:25 +0900

    get example c file of fork & pipe

--- src/execute_command.c
+++ src/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 17:50:50 by yoonsele          #+#    #+#             */
-/*   Updated: 2023/02/24 18:00:36 by yoonsele         ###   ########.fr       */
+/*   Updated: 2023/02/24 19:43:17 by yoonsele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	execute_command(t_data *data, char **env)
 {
+	int		pipefd[2];
+	pid_t	cpid;
 	execve(data->path_cmd1, data->cmd_options1, env);
 
 	return (0);
