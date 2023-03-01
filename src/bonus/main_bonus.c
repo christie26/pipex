3abb900579864f48931f2ae5241c7c26e075562a
yoonsele <yoonseo9800@gmail.com>
2023-03-01 18:21:03 +0900

    Lisibilité set_bonus

--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 16:14:46 by yoonsele          #+#    #+#             */
-/*   Updated: 2023/03/01 17:39:49 by yoonsele         ###   ########.fr       */
+/*   Updated: 2023/03/01 18:13:12 by yoonsele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int	main(int ac, char **av, char **env)
 	ft_err_msg(ac < 5, "Invalid number of arguments", __FILE__, __LINE__);
 	data.number = ac - 3;
 	data.cmd = (char **)malloc(sizeof(char *) * data.number);
+	ft_err_msg(!data.cmd, "Fail to malloc();", __FILE__, __LINE__);
 	data.cmd_options = (char ***)malloc(sizeof(char **) * data.number);
+	ft_err_msg(!data.cmd_options, "Fail to malloc();", __FILE__, __LINE__);
 	pipex_set(av, env, &data);
 	pipex_execute(&data, env);
 	return (EXIT_SUCCESS);
--- src/bonus/set_bonus.c
+++ src/bonus/set_bonus.c
@@ -6,16 +6,16 @@
 /*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 16:14:36 by yoonsele          #+#    #+#             */
-/*   Updated: 2023/03/01 17:39:52 by yoonsele         ###   ########.fr       */
+/*   Updated: 2023/03/01 18:18:42 by yoonsele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/pipex_bonus.h"
+#include <stdlib.h>
 
-char	**get_path(char **env)
+void	get_path(char **env, char ***path)
 {
 	int		i;
-	char	**path;
 	char	*tmp;
 
 	while (*env++)
@@ -23,21 +23,20 @@ char	**get_path(char **env)
 		if (!ft_strncmp(*env, "PATH", 4))
 			break ;
 	}
-	path = ft_split((*env + 5), ':');
-	ft_err_msg(!path, "Fail to malloc();", __FILE__, __LINE__);
+	*path = ft_split((*env + 5), ':');
+	ft_err_msg(!(*path), "Fail to malloc();", __FILE__, __LINE__);
 	i = 0;
-	while (path[i])
+	while ((*path)[i])
 	{
-		tmp = path[i];
-		path[i] = ft_strjoin(path[i], "/");
-		ft_err_msg(!path[i], "Fail to malloc();", __FILE__, __LINE__);
+		tmp = (*path)[i];
+		(*path)[i] = ft_strjoin((*path)[i], "/");
+		ft_err_msg(!(*path)[i], "Fail to malloc();", __FILE__, __LINE__);
 		free(tmp);
 		i++;
 	}
-	return (path);
 }
 
-int	get_cmd(t_data *data, char **av)
+void	get_cmd(t_data *data, char **av)
 {
 	int	i;
 
@@ -45,12 +44,11 @@ int	get_cmd(t_data *data, char **av)
 	while (i < data->number)
 	{
 		data->cmd_options[i] = ft_split(av[i + 2], ' ');
-		if (!data->cmd_options[i])
-			return (EXIT_FAILURE);
+		ft_err_msg(!data->cmd_options[i], "Fail to malloc();", \
+				__FILE__, __LINE__);
 		data->cmd[i] = data->cmd_options[i][0];
 		i++;
 	}
-	return (EXIT_SUCCESS);
 }
 
 char	*check_access(char *cmd, char **path)
@@ -59,13 +57,8 @@ char	*check_access(char *cmd, char **path)
 	char	*path_cmd;
 
 	i = 0;
-	if (*cmd == '/')
-	{
-		if (access(cmd, X_OK) == 0)
-			return (cmd);
-		else
-			return (0);
-	}
+	if (access(cmd, X_OK) == 0)
+		return (cmd);
 	while (path[i])
 	{
 		path_cmd = ft_strjoin(path[i], cmd);
@@ -77,7 +70,7 @@ char	*check_access(char *cmd, char **path)
 	return (0);
 }
 
-int	get_path_cmd(t_data *data, char **path)
+void	get_path_cmd(t_data *data, char **path)
 {
 	char	*tmp;
 	int		i;
@@ -87,23 +80,20 @@ int	get_path_cmd(t_data *data, char **path)
 	{
 		tmp = data->cmd[i];
 		data->cmd[i] = check_access(data->cmd[i], path);
-		if (!data->cmd[i])
-			return (EXIT_FAILURE);
+		ft_err_msg(!(data->cmd[i]), "Invalid command !", __FILE__, __LINE__);
 		free(tmp);
 		i++;
 	}
-	return (EXIT_SUCCESS);
 }
 
 int	pipex_set(char **av, char **env, t_data *data)
 {
 	char	**path;
 
-	path = get_path(env);
+	get_path(env, &path);
 	data->infile = av[1];
 	data->outfile = av[data->number + 2];
-	ft_err_sys(get_cmd(data,av), __FILE__, __LINE__);
-	ft_err_msg(get_path_cmd(data, path), "Invalid command", \
-			__FILE__, __LINE__);
+	get_cmd(data, av);
+	get_path_cmd(data, path);
 	return (0);
 }
